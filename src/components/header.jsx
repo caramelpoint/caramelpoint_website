f62e0dea21c5df6b79eceec8ded73f2ca2f371f5
Facu Rossi <facundo@caramelpoint.com>
2021-02-08 15:01:10 -0300

    Update styles

---
+++ src/components/header.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Header() {
+  return <div className="w-60 h-60 bg-black"></div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import '../sass/layout.scss'
+import Header from '../components/header'
 // data
 const links = [
   {
@@ -50,14 +51,15 @@ const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
-      <h1>
+      <Header />
+      {/* <h1>
         Congratulations
         <br />
         <span>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
-      </h1>
+      </h1> */}
       <p>
         Edit <code>src/pages/index.js</code> to see this page update in real-time.{' '}
         <span role="img" aria-label="Sunglasses smiley emoji">
@@ -89,24 +91,24 @@ const IndexPage = () => {
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
-      <ul class="grid grid-cols-2 gap-4">
+      <ul className="grid grid-cols-2 gap-4">
         <li>
-          <div class="shadow-sm bg-white rounded-lg h-18"></div>
+          <div className="shadow-sm bg-white rounded-lg h-18"></div>
         </li>
         <li>
-          <div class="shadow bg-white rounded-lg h-18"></div>
+          <div className="shadow bg-white rounded-lg h-18"></div>
         </li>
         <li>
-          <div class="shadow-md bg-white rounded-lg h-18"></div>
+          <div className="shadow-md bg-white rounded-lg h-18"></div>
         </li>
         <li>
-          <div class="shadow-lg bg-white rounded-lg h-18"></div>
+          <div className="shadow-lg bg-white rounded-lg h-18"></div>
         </li>
         <li>
-          <div class="shadow-xl bg-white rounded-lg h-18"></div>
+          <div className="shadow-xl bg-white rounded-lg h-18"></div>
         </li>
         <li>
-          <div class="shadow-2xl bg-white rounded-lg h-18"></div>
+          <div className="shadow-2xl bg-white rounded-lg h-18"></div>
         </li>
       </ul>
     </main>
